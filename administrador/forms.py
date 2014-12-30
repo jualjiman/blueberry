27a2888ac030e1bb53982c5050416939f4bf3f4c
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2014-12-30 11:35:00 -0600

    widget

--- administrador/forms.py
+++ administrador/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'placeholder': 'Nombre*','class':'form-control validate[required]'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email*','class':'form-control validate[required]'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email*','class':'form-control validate[required,custom[email]]'}))
     subject = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Asunto','class':'form-control'}))
 
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Mensaje','class':'form-control validate[required]','rows':'7'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Mensaje','class':'form-control validate[required]','rows':'5'}))
