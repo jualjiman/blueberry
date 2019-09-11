527ac19109cc0d8368ac33b93ef19dd50e7b476d
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2019-09-11 10:25:31 -0500

    Adding sitemap page to website

---
+++ blueberry/sitemaps.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from django.contrib.sitemaps import Sitemap
+from django.core.urlresolvers import reverse
+
+
+class StaticViewSitemap(Sitemap):
+    def location(self, item):
+        return reverse(item)
+
+    def lastmod(self, item):
+            return datetime(2019, 1, 1, 1, 1, 1)
+
+
+class OtherStaticViewSitemap(StaticViewSitemap):
+    priority = 0.8
+    def items(self):
+        return [
+            'nosotros',
+            'servicios',
+            'eventos',
+            'book',
+            'contacto'
+        ]
+
+
+class HomeStaticViewSitemap(StaticViewSitemap):
+    priority = 1.0
+    def items(self):
+        return ['home']
--- blueberry/urls.py
+++ blueberry/urls.py
@@ -1,13 +1,17 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
 from django.conf import settings
+from django.conf.urls import include, patterns, url
+from django.contrib import admin
+
+from blueberry.sitemaps import HomeStaticViewSitemap, OtherStaticViewSitemap
+
 admin.autodiscover()
 
+sitemaps = {
+    'home': HomeStaticViewSitemap,
+    'other': OtherStaticViewSitemap
+}
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'blueberry.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    #url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', 'administrador.views.home', name='home'),
@@ -16,8 +20,7 @@ urlpatterns = patterns('',
     url(r'^eventos/$', 'administrador.views.eventos', name='eventos'),
     url(r'^book/$', 'administrador.views.book', name='book'),
     url(r'^contacto/$', 'administrador.views.contacto', name='contacto'),
-    url(r'^mas/$', 'administrador.views.mas', name='mas'),
-    url(r'^contactame/$', 'administrador.views.contactame', name='contactame'),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps})
 )
 
 urlpatterns += patterns('',url(r'^/webapps/fortalezac/fortaleza/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),)
