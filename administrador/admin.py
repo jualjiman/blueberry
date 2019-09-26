a0c4c5b0e5a2652fae62dc40d92cff8981f22388
Juan Alberto Jimenez Angel <jualjiman@gmail.com>
2019-09-26 11:13:53 -0500

    ordering edecanes by priority

--- administrador/admin.py
+++ administrador/admin.py
@@ -4,49 +4,56 @@ from sorl.thumbnail.shortcuts import get_thumbnail
 
 # Register your models here.
 
+
 class SliderAdmin(admin.ModelAdmin):
-	list_display = ('imagen_slider','titulo','descripcion','activo',)
-	search_fields = ['titulo','descripcion',]
+    list_display = ('imagen_slider', 'titulo', 'descripcion', 'activo',)
+    search_fields = ['titulo', 'descripcion', ]
 
-	def imagen_slider(self,obj):
-		return '<img src="%s" />' % get_thumbnail(obj.imagen,'100x60', crop='center').url #format='PNG', quality=99
+    def imagen_slider(self, obj):
+        return '<img src="%s" />' % get_thumbnail(obj.imagen, '100x60', crop='center').url  # format='PNG', quality=99
 
-	imagen_slider.allow_tags = True
+    imagen_slider.allow_tags = True
+
 
 class EventoAdmin(admin.ModelAdmin):
-	list_display = ('imagen_evento','lugar','servicio','posicion','activo',)
-	search_fields = ['lugar','servicio',]
+    list_display = ('imagen_evento', 'lugar', 'servicio', 'posicion', 'activo', )
+    search_fields = ['lugar', 'servicio', ]
 
-	def imagen_evento(self,obj):
-		return '<img src="%s" />' % get_thumbnail(obj.imagen,'100x60', crop='center').url #format='PNG', quality=99
+    def imagen_evento(self, obj):
+        return '<img src="%s" />' % get_thumbnail(obj.imagen, '100x60', crop='center').url  # format='PNG', quality=99
 
-	imagen_evento.allow_tags = True
+    imagen_evento.allow_tags = True
+
 
 class EdecanAdmin(admin.ModelAdmin):
-	list_display = ('imagen_edecan','activo','prioridad','alineacion',)
+    list_display = ('imagen_edecan', 'activo', 'prioridad', 'alineacion',)
 
-	def imagen_edecan(self,obj):
-		return '<img src="%s" />' % get_thumbnail(obj.imagen,'100x100', crop='center').url #format='PNG', quality=99
+    def imagen_edecan(self, obj):
+        return '<img src="%s" />' % get_thumbnail(obj.imagen, '100x100', crop='center').url  # format='PNG', quality=99
 
-	def get_queryset(self, request):
-		qs = super(self, EdecanAdmin).get_queryset(request)
+    def get_queryset(self, request):
+        qs = super(self, EdecanAdmin).get_queryset(request)
         return qs
 
-	imagen_edecan.allow_tags = True
+    imagen_edecan.allow_tags = True
+
 
 class TestimonialAdmin(admin.ModelAdmin):
-	list_display = ('cita','autor','puesto','activo',)
-	search_fields = ['cita','autor','puesto']
+    list_display = ('cita', 'autor', 'puesto', 'activo',)
+    search_fields = ['cita', 'autor', 'puesto']
+
 
 class MensAdmin(admin.ModelAdmin):
-	list_display = ('nombre','email','mensaje','fecha')
+    list_display = ('nombre', 'email', 'mensaje', 'fecha')
+
 
 class Messages(admin.ModelAdmin):
-	list_display = ('sender','recipient','subject','nattachments','fecha')
+    list_display = ('sender', 'recipient', 'subject', 'nattachments', 'fecha')
 
-admin.site.register(Mensaje,MensAdmin)
-admin.site.register(Email,Messages)
-admin.site.register(Slider,SliderAdmin)
-admin.site.register(Evento,EventoAdmin)
-admin.site.register(Edecan,EdecanAdmin)
-admin.site.register(Testimonial,TestimonialAdmin)
+
+admin.site.register(Mensaje, MensAdmin)
+admin.site.register(Email, Messages)
+admin.site.register(Slider, SliderAdmin)
+admin.site.register(Evento, EventoAdmin)
+admin.site.register(Edecan, EdecanAdmin)
+admin.site.register(Testimonial, TestimonialAdmin)
